f8f014603d37d2d79ba9a8afea9fe2fa3e0a07b2
LucasPAlvarez <48615149+LucasPAlvarez@users.noreply.github.com>
2019-08-23 20:56:22 -0300

    esqueleto
    aca cargo los archivos separados con una descripcion breve para poder guiarnos

---
+++ viajante/busAG.py
@@ -0,0 +1,6 @@
+import carga
+
+def busquedaAG():
+	#esta funcion se encargara de buscar la ruta mas corta utilizando algoritmos geneticos
+	#es posible que se necesite hacermas archivos que se encargen de la poblacion y genes
+	pass
---
+++ viajante/busHeu.py
@@ -0,0 +1,14 @@
+import carga
+
+distancias = carga.cargadatos()
+
+def BusquedaHeuristicaDeUnOrigen(origen,array):
+	#esta funcion buscara la distancia mas chica desde un origen dado
+	#para buscarlo vera cual es la capital mas cercana de la posicion en la que se encuentra
+	pass
+
+def outputBusqueda(origen, array):
+	sol = BusquedaHeuristicaDeUnOrigen(origen, array)
+	#despues de guardar la solucion en sol esta funcion pasara el resultado a unas funciones de output.py
+	#para mostrarlo en pantalla
+	pass
