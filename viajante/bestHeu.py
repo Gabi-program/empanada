35a3c5c39c97cec41f9e95c007b3e04215b795f8
Gabi-program <54423040+Gabi-program@users.noreply.github.com>
2019-08-29 09:11:28 -0300

    Búsqueda heuristica

--- viajante/bestHeu.py
+++ viajante/bestHeu.py
@@ -1,6 +1,17 @@
 import busHeu
 
-def busquedaHeuTotal():
+def busquedaHeuTotal(Capitales):
 	#esta funcion utilizara la busqueda heuristica por origen y las comparara
 	# devolviendo la mejor de ellas
-	pass
+	mintot=1000000
+	for i in range(len((Capitales))-1):
+		resultado=busHeu.BusquedaHeuristicaDeUnOrigen(i+1,Capitales)
+		if resultado.get("total")<mintot:
+			mintot=resultado.get("total")
+			print(Capitales[i+1][0])
+			print(mintot)
+			print("entro")
+			mejorHeu=i+1
+			output=resultado
+	print("el mejor camino para recorrer todas las capitales es partiendo de ",Capitales[mejorHeu][0]," y recorre ",mintot,"km")
+	return output
--- viajante/busHeu.py
+++ viajante/busHeu.py
@@ -1,14 +1,40 @@
 import carga
+import Output
 
-distancias = carga.cargadatos()
+def BuscarIndice(ori,Capitales):
+	inx="nada"
+	for i in range(len(Capitales)):
+		if Capitales[i][0]==ori:
+			inx=i 
+			break
+	return inx
 
-def BusquedaHeuristicaDeUnOrigen(origen,array):
+def BusquedaHeuristicaDeUnOrigen(origen,Capitales):
 	#esta funcion buscara la distancia mas chica desde un origen dado
 	#para buscarlo vera cual es la capital mas cercana de la posicion en la que se encuentra
-	pass
+	Resultados=[origen]
+	total=0
+	CapActual=origen
+	minimo=999999
+	while (len(Resultados))!=len(Capitales)-1:
+		for i in range(len(Capitales[CapActual])-1):
+			if (i+1)!=CapActual:
+				if (int(Capitales[CapActual][i+1])<minimo) and ((i+1) not in Resultados):
+					minimo=int(Capitales[CapActual][i+1])
+					indice=i+1
+		CapActual=indice
+		total=total+minimo
+		minimo=999999
+		Resultados.append(indice)
+	#Resultados.append(origen)
+	total=total+int(Capitales[indice][origen])
+	output={"orden":Resultados,"total":total}
+	return output
 
-def outputBusqueda(origen, array):
+
+'''def outputBusqueda(origen, array):
 	sol = BusquedaHeuristicaDeUnOrigen(origen, array)
 	#despues de guardar la solucion en sol esta funcion pasara el resultado a unas funciones de output.py
-	#para mostrarlo en pantalla
-	pass
+	#para mostrarlo en pantalla,supongo que sera el mapa
+	pass'''
+
